2daf4ea478320997a5bcdc9f682cfc1d6b8ed2fd
HeySomtee <ilosomto2000@gmail.com>
2023-01-02 20:51:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const back_img = document.getElementById("zIndex");
+
+function change(e) {
+    
+    if (back_img.src = "https://pbs.twimg.com/media/FfUXolSUUAAXYT5?format=jpg&name=small") {
+        setInterval(() => {
+            back_img.src = "https://c4.wallpaperflare.com/wallpaper/1012/835/620/andres-iniesta-fc-barcelona-wallpaper-preview.jpg";
+        }, 4000);
+    }
+    if (back_img.src = "https://c4.wallpaperflare.com/wallpaper/1012/835/620/andres-iniesta-fc-barcelona-wallpaper-preview.jpg") {
+        setInterval(() => {
+            back_img.src = "https://media.gettyimages.com/id/102811165/photo/andres-iniesta-of-spain-celebrates-scoring-his-sides-first-goal-during-the-2010-fifa-world-cup.jpg?s=612x612&w=0&k=20&c=IBAI_ZqbGhTRmLYq34wImKO4d4uzVnAKxONE0g9DVYE="
+        }, 4000);
+    }
+    if (back_img.src = "https://media.gettyimages.com/id/102811165/photo/andres-iniesta-of-spain-celebrates-scoring-his-sides-first-goal-during-the-2010-fifa-world-cup.jpg?s=612x612&w=0&k=20&c=IBAI_ZqbGhTRmLYq34wImKO4d4uzVnAKxONE0g9DVYE=")
+    {
+        setInterval(() => {
+            back_img.src = "https://media.gettyimages.com/id/463194658/photo/andres-iniesta-of-fc-barcelona-celebrates-after-scoring-his-teams-second-goal-during-the-copa.jpg?s=612x612&w=0&k=20&c=dHu1vKNyssO36vah5Y5Ya4g3M2e9BWS26-8zJHgE1-E="
+        }, 4000);
+    }
+
+
+    if (back_img.src = "https://media.gettyimages.com/id/463194658/photo/andres-iniesta-of-fc-barcelona-celebrates-after-scoring-his-teams-second-goal-during-the-copa.jpg?s=612x612&w=0&k=20&c=dHu1vKNyssO36vah5Y5Ya4g3M2e9BWS26-8zJHgE1-E=") {
+        setInterval(() => {
+            back_img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyGuYN97iwVI5oPMdy4FrSr69SCSd0skHJ0A&usqp=CAU"
+        }, 4000);
+    }
+
+   
+}
+
+change();
